e9ed11e8c6749550681753eef67ad4ec2d1beb3a
kilyy <73783249+kilyy@users.noreply.github.com>
2025-11-26 07:29:26 +0100

    Implement data loading for training and validation
    Added data loading for training and validation datasets using CellDataset and DataLoader.

--- Code/Modele_pytorch.py
+++ Code/Modele_pytorch.py
@@ -16,20 +16,31 @@ import Pathlib as Path
 # --- CHARGEMENT DES DONNEES --- #
 ##################################
 # charger les donnees + les rendres accessibles pour pytorch
+ROOT_NPY_DIR = 'processed_data' 
+TRAIN_NPY_DIR = os.path.join(ROOT_NPY_DIR, 'train')
+VAL_NPY_DIR = os.path.join(ROOT_NPY_DIR, 'validation')
 
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
+BATCH_SIZE = 42 # Nb images par lot (je savais pas quoi mettre mdrr, à tester)
 
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor()
-)
+if __name__ == "__main__":
+# Créer les instances de Dataset pour le train et la validation
+    train_dataset = CellDataset(TRAIN_NPY_DIR)
+    val_dataset = CellDataset(VAL_NPY_DIR)
+    
+    train_loader = DataLoader(
+            root = train_dataset, 
+            batch_size = BATCH_SIZE, 
+            shuffle=True, 
+            num_workers=NUM_WORKERS
+        )
+        
+        # Validation : shuffle=False parce qu'on s'en fou de  l'ordre 
+    val_loader = DataLoader(
+            root = val_dataset, 
+            batch_size = BATCH_SIZE, 
+            shuffle=False, 
+            num_workers=NUM_WORKERS
+        )
 
 ########################
 # --- MODÈLE (CNN) --- #
