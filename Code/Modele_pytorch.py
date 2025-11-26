7e8c81911dde42712f441c2fd043a63d7f2b4aa8
kilyy <73783249+kilyy@users.noreply.github.com>
2025-11-26 07:54:33 +0100

    Add DataSet_Cell class for loading cell images

--- Code/Modele_pytorch.py
+++ Code/Modele_pytorch.py
@@ -22,24 +22,55 @@ VAL_NPY_DIR = os.path.join(ROOT_NPY_DIR, 'validation')
 
 BATCH_SIZE = 42 # Nb images par lot (je savais pas quoi mettre mdrr, à tester)
 
+class DataSet_Cell(DataSet):
+def __init__(self, chemin_principal):
+        # Initialisation
+        self.fichiers_listes = []
+        self.map_classes = {'hem': 0, 'all': 1}
+        
+        # On va parcourir les deux sous-dossiers ('hem' et 'all').
+        for nom_classe in ['hem', 'all']:
+            chemin_classe = os.path.join(chemin_principal, nom_classe)
+            
+            # On vérifie si le dossier existe
+            if os.path.isdir(chemin_classe):
+                # On liste les fichiers .npy dans ce dossier 
+                for nom_fichier in os.listdir(chemin_classe):
+                    if nom_fichier.endswith('.npy'):
+                        # On stocke le chemin complet de chaque fichier
+                        chemin_complet = os.path.join(chemin_classe, nom_fichier)
+                        self.fichiers_listes.append(chemin_complet)
+def __getitem__(self, idx):
+        chemin_complet = self.fichiers_listes[idx]
+        # Lecture du fichier .npy
+        img_array = np.load(chemin_complet) 
+        
+        # On extrait la classe à partir du chemin complet
+        label_str = Path(chemin_complet).parent.name 
+        label_int = self.map_classes.get(label_str) 
+        
+        # Convertit la matrice NumPy de l'image (H, L, C) en Tenseur PyTorch (C, H, L) 
+        tenseur_img = torch.from_numpy(img_array).permute(2, 0, 1).float()
+        tenseur_label = torch.tensor(label_int, dtype=torch.long)
+        
+        return tenseur_img, tenseur_label
+                    
+
 if __name__ == "__main__":
 # Créer les instances de Dataset pour le train et la validation
-    train_dataset = CellDataset(TRAIN_NPY_DIR)
-    val_dataset = CellDataset(VAL_NPY_DIR)
+    train_dataset = DataSet_Cell(TRAIN_NPY_DIR)
+    val_dataset = DataSet_Cell(VAL_NPY_DIR)
     
     train_loader = DataLoader(
             root = train_dataset, 
             batch_size = BATCH_SIZE, 
             shuffle=True, 
-            num_workers=NUM_WORKERS
         )
         
-        # Validation : shuffle=False parce qu'on s'en fou de  l'ordre 
     val_loader = DataLoader(
             root = val_dataset, 
             batch_size = BATCH_SIZE, 
-            shuffle=False, 
-            num_workers=NUM_WORKERS
+            shuffle=False, #shuffle=False parce qu'on s'en fou de  l'ordre 
         )
 
 ########################
