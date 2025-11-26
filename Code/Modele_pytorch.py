2172cb340f0a718118610c9f968fbce163de46ac
kilyy <73783249+kilyy@users.noreply.github.com>
2025-11-26 07:05:48 +0100

    Import FashionMNIST dataset and required modules
    Added imports and loading for FashionMNIST dataset

--- Code/Modele_pytorch.py
+++ Code/Modele_pytorch.py
@@ -4,7 +4,10 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import Pathlib as Path
@@ -13,6 +16,20 @@ import Pathlib as Path
 # --- CHARGEMENT DES DONNEES --- #
 ##################################
 # charger les donnees + les rendres accessibles pour pytorch
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
 
 ########################
 # --- MODÈLE (CNN) --- #
