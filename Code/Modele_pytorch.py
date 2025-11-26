e6f5e07997fa5732a23d19ecf3a254cdb18f22f7
kilyy <73783249+kilyy@users.noreply.github.com>
2025-11-26 06:55:25 +0100

    Add structure for CNN model in Modele_pytorch.py
    Added sections for data loading, model definition, training, and evaluation.

--- Code/Modele_pytorch.py
+++ Code/Modele_pytorch.py
@@ -1,1 +1,28 @@
+########################
+# --- IMPORATTIONS --- #
+########################
+import torch
+import numpy as np
+import os
+import Pathlib as Path
 
+##################################
+# --- CHARGEMENT DES DONNEES --- #
+##################################
+# charger les donnees + les rendres accessibles pour pytorch
+
+########################
+# --- MODÈLE (CNN) --- #
+########################
+# Partie pour faire notre modele. je propose de partir sur un CNN 
+
+
+########################
+# --- ENTRAINEMENT --- #
+########################
+# Partie pour entrainer notre modele 
+
+######################
+# --- EVALUATION --- #
+######################
+# Partie pour evaluer notre modele 
